d09fc78f10e214e2601d4cd43362d391cb8b623b
RafaelR4mos <rafaelramos0401@gmail.com>
2025-06-29 18:24:38 -0300

    chore: initial project setup

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata: Metadata = {
+  title: "App | Votação",
+  description: "App de votação de cooperativa",
+  authors: { name: "Rafael Ramos" },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
