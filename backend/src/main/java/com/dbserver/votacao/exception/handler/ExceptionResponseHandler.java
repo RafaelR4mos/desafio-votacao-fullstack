da06ec2e7be95e27ecc6e9934cf0d644188a7d8e
RafaelR4mos <rafaelramos0401@gmail.com>
2025-06-29 17:07:24 -0300

    feat: add more exception validations

--- backend/src/main/java/com/dbserver/votacao/exception/handler/ExceptionResponseHandler.java
+++ backend/src/main/java/com/dbserver/votacao/exception/handler/ExceptionResponseHandler.java
@@ -1,10 +1,11 @@
 package com.dbserver.votacao.exception.handler;
 
-import com.dbserver.votacao.exception.RegraDeNegocioException;
+import com.dbserver.votacao.exception.BusinessRuleException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,19 @@ import java.util.Map;
 @RestControllerAdvice
 public class ExceptionResponseHandler {
 
+    // In case, client not send a required body, with @RequestBody
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponseHandler> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        log.error("HttpMessageNotReadableException occurred: {}", ex.getMessage());
+        ErrorResponseHandler errorResponse = ErrorResponseHandler.builder().statusCode(HttpStatus.BAD_REQUEST.value()).
+                error("MESSAGE_NOT_READABLE").
+                message("Corpo da solicitação com JSON inválido.").
+                timestamp(System.currentTimeMillis()).build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    // In case, a validation fails in fields
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseHandler> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         log.error("MethodArgumentNotValidException occurred: {}", ex.getMessage());
@@ -39,8 +53,9 @@ public class ExceptionResponseHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
-    @ExceptionHandler(RegraDeNegocioException.class)
-    public ResponseEntity<ErrorResponseHandler> handleRegraDeNegocioException(RegraDeNegocioException ex) {
+    // Generic Exception - Used mainly for application business rules
+    @ExceptionHandler(BusinessRuleException.class)
+    public ResponseEntity<ErrorResponseHandler> handleRegraDeNegocioException(BusinessRuleException ex) {
         log.error("RegraDeNegocioException occurred: {}", ex.getMessage());
         ErrorResponseHandler errorResponse = ErrorResponseHandler.builder().statusCode(HttpStatus.BAD_REQUEST.value())
                 .error("RegraDeNegocioException").message(ex.getMessage()).timestamp(System.currentTimeMillis()).build();
@@ -58,6 +73,8 @@ public class ExceptionResponseHandler {
             message = "Não é possível utilizar este CPF para completar o cadastro";
         } else if (ex.getMessage().contains("associate_email_key")) {
             message = "Não é possível utilizar este e-mail para completar o cadastro";
+        } else if (ex.getMessage().contains("agenda_slug_key")) {
+            message = "Já existe uma pauta cadastrada com exatamente este título";
         }
 
         ErrorResponseHandler errorResponse = ErrorResponseHandler.builder().statusCode(HttpStatus.CONFLICT.value())
