f08e61fbc1529e5ae67e74975356469ca1c0f92a
RafaelR4mos <rafaelramos0401@gmail.com>
2025-06-29 14:29:34 -0300

    chore: configure bean for `modelMapper`

---
+++ backend/src/main/java/com/dbserver/votacao/configuration/ModelMapperConfig.java
@@ -0,0 +1,19 @@
+package com.dbserver.votacao.configuration;
+
+import org.modelmapper.Conditions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public org.modelmapper.ModelMapper modelMapper() {
+        org.modelmapper.ModelMapper modelMapper = new org.modelmapper.ModelMapper();
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        modelMapper.createTypeMap(ArrayList.class, List.class);
+        return modelMapper;
+    }
+}
